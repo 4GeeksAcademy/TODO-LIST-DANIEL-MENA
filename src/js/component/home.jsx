e3af64c216b891e671cca872bff338d3c4fe1316
DANNIMENNA89 <146239929+DANNIMENNA89@users.noreply.github.com>
2023-12-27 08:14:43 +0000

    Proyecto TODO LIST React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,13 +3,17 @@ import React, { useState } from "react";
 //create your first component
 const Home = () => {
 	const [add, setAdd] = useState("");
-	const [list, setList] = useState([]);
+	const [list, setList] = useState(JSON.parse(localStorage.getItem("myList")) || []);
 	const [hoverIndex, setHoverIndex] = useState(null);
 
+	
 const keyIntro = (e) =>{
 		if(e.code==="Enter"){
 			e.preventDefault();
+			let aux=[];
+			aux=list.concat(add);
 			setList(list.concat(add));
+			localStorage.setItem("myList", JSON.stringify(aux));
 			setAdd("");
 		}									
 }
@@ -19,7 +23,11 @@ const keyIntro = (e) =>{
   }
 
   const deleteList = (index) =>{
+	let aux = [];
+	aux=list.filter((item,i) => i != index);
 	setList(list.filter((item,i) => i != index));
+	localStorage.removeItem("myList");
+	localStorage.setItem('myList', JSON.stringify(aux));
   }
 
   const handleMouseLeave = (index) =>{
@@ -28,7 +36,7 @@ const keyIntro = (e) =>{
 
 
 	const listItems = list.map((e, index) =>
-		<li key={index} className="bg-white list-unstyled d-flex justify-content-between border border-dark-subtle border-top-0" style={{width:"180.8px", height:"24px"}} onMouseEnter={() => handleMouseEnter(index)} onMouseLeave={() => handleMouseLeave(index)}>
+		<li key={index} className="bg-white list-unstyled d-flex justify-content-between border border-dark-subtle border-top-0" onMouseEnter={() => handleMouseEnter(index)} onMouseLeave={() => handleMouseLeave(index)}>
 			<span className="text-secondary ps-2" style={{fontFamily: "Times New Roman", fontSize: "10px", cursor: "default " }}>{e}</span>
 			<span className={`text-secondary pe-3 ${hoverIndex===index ? "d-flex" : "d-none"}`}  onClick={()=>deleteList(index)} style={{fontFamily: "Times New Roman", fontSize: "10px", cursor: "default " }}>X</span>
 		</li>
@@ -37,14 +45,14 @@ const keyIntro = (e) =>{
 
 	return (
 			<form className="container" onSubmit={keyIntro}>
-				<div className="m-auto bg-danger bg-gradient bg-opacity-25" style={{width:"250px"}}>
+				<div className="m-auto bg-danger bg-gradient bg-opacity-25 col-5">
 					<p className="text-center my-1 text-danger text-opacity-50 " style={ {fontFamily: "Times New Roman", fontSize: "50px", cursor: "default " }  }>todos</p>
-					<div className="d-flex flex-column align-items-center">					
-						<input value={add} className="form-control border border-dark-subtle rounded-0 text-secondary" onChange={(e)=>setAdd(e.target.value)} onKeyDown={keyIntro} style={{width:"180.8px", height:"24px", boxShadow: "0px 0px 0px rgba(0, 0, 0, 0)", fontFamily: "Times New Roman", fontSize: "10px", cursor: "default " }} placeholder="What needs to be done?"/>				
-						<ul className="ps-0">{listItems}</ul>				
+					<div className="d-flex flex-column align-items-center col-11 m-auto">					
+						<input value={add} className="form-control border border-dark-subtle rounded-0 text-secondary" onChange={(e)=>setAdd(e.target.value)} onKeyDown={keyIntro} style={{ boxShadow: "0px 0px 0px rgba(0, 0, 0, 0)", fontFamily: "Times New Roman", fontSize: "10px", cursor: "default " }} placeholder="What needs to be done?"/>				
+						<ul className="ps-0 col-12 m-auto">{listItems}</ul>				
 					</div>
-					<div>
-						<p className="ps-2 text-secondary text-opacity-75 border border-top-dark-subtle" style={{fontFamily: "Times New Roman", fontSize: "8px" }}>{list.length} item left</p>
+					<div className="py-1 border border-top-dark-subtle">
+						<p className="text-secondary text-opacity-75 mb-0" style={{fontFamily: "Times New Roman", fontSize: "8px" }}>{list.length} item left</p>
 					</div>
 				</div>				
 			</form>
